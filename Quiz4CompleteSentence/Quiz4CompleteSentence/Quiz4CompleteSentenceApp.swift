25034da993b19a77f719c00a3775b2759b9df7e1
niatee <127348431+niatee@users.noreply.github.com>
2023-04-09 23:04:37 -0500

    Add files via upload

---
+++ Quiz4CompleteSentence/Quiz4CompleteSentence/Quiz4CompleteSentenceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz4CompleteSentenceApp.swift
+//  Quiz4CompleteSentence
+//
+//  Created by Niyati Belathur on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Quiz4CompleteSentenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
